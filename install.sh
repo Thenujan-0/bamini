96ddc90d5d20f2e59bb07959ff912198e77e615b
Thenujan-0 <85059778+Thenujan-0@users.noreply.github.com>
2022-03-01 11:08:13 +0530

    now script automatically adds environment variables

--- install.sh
+++ install.sh
@@ -7,7 +7,38 @@ if is_user_root; then
     cp tamil-bamini.conf /usr/share/fcitx5/inputmethod/tamil-bamini.conf
     mkdir -p /usr/share/fcitx5/table/
     cp tamil-bamini.main.dict /usr/share/fcitx5/table/tamil-bamini.main.dict
+    #source this file to check if the environment variables are already configured
+    source /etc/environment
+
+    if [ "$GTK_IM_MODULE" = "" ];then
+        echo "GTK_IM_MODULE=fcitx">> /etc/environment
+    else
+        echo "GTK_IM_MODULE is already set"
+    fi
+
+
+
+    if [ "$QT_IM_MODULE" = "" ];then
+        echo "QT_IM_MODULE=fcitx">> /etc/environment
+    else
+        echo "QT_IM_MODULE is already set"
+    fi
+
+
+
+
+    if [ "$XMODIFIERS" = "" ];then
+        echo "XMODIFIERS=@im=fcitx">> /etc/environment
+    else
+        echo "XMODIFIERS is already set"
+    fi
+    
+    
     echo 'Installation success!'
+    
+    
+    
+    
     exit 0 # implicit, here it serves the purpose to be explicit for the reader
 else
     echo 'You need sudo permissions to install.' >&2
